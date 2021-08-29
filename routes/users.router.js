41554323c6c12b64cd904693f3c12832a275f1a9
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-08-30 01:46:44 +0530

    added user routes for updating, deleting & following a specific user

--- routes/users.router.js
+++ routes/users.router.js
@@ -4,64 +4,131 @@ const bcrypt = require("bcryptjs");
 const { User } = require("../models/user.model");
 const { authVerify } = require("../middlewares/authVerify");
 
-router.put("/:_id", authVerify, async (req, res) => {
-  const { _id } = req.params;
-  const user = req.user;
-  if (_id !== user._id) {
-    return res
-      .status(200)
-      .json({
+router
+  .route("/:_id")
+  // get a user
+  .get(async (req, res) => {
+    const { _id } = req.params;
+    try {
+      const findUserById = await User.findById(_id);
+      const { password, ...rest } = findUserById._doc;
+      res.status(201).json({ success: true, message: "User Found", rest });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Something Went Wrong,",
+      });
+    }
+  })
+  // update User
+  .put(authVerify, async (req, res) => {
+    const { _id } = req.params;
+    const user = req.user;
+    if (_id !== user._id) {
+      return res.status(200).json({
         success: false,
         message: "You Can't Update Other User's Credentials",
       });
-  }
-  try {
-    if (req.body.password) {
-      const salt = await bcrypt.genSalt(10);
-      req.body.password = await bcrypt.hash(req.body.password, salt);
     }
-    if (req.body.username) {
-      const findUserByUsername = await User.findOne({
-        username: req.body.username,
-      });
-      if (findUserByUsername) {
-        return res
-          .status(401)
-          .json({ success: false, message: "Username ALready Exists" });
+    try {
+      if (req.body.password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
       }
+      if (req.body.username) {
+        const findUserByUsername = await User.findOne({
+          username: req.body.username,
+        });
+        if (findUserByUsername) {
+          return res
+            .status(401)
+            .json({ success: false, message: "Username ALready Exists" });
+        }
+      }
+
+      if (req.body.email) {
+        const findUserByEmail = await User.findOne({
+          email: req.body.email,
+        });
+        if (findUserByEmail) {
+          return res
+            .status(401)
+            .json({ success: false, message: "Email ALready Exists" });
+        }
+      }
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: "Something Went Wrong, Please Try Afetr Sometime",
+      });
     }
 
-    if (req.body.email) {
-      const findUserByEmail = await User.findOne({
-        email: req.body.email,
+    try {
+      const updateUser = await User.findByIdAndUpdate(user._id, {
+        $set: req.body,
       });
-      if (findUserByEmail) {
-        return res
-          .status(401)
-          .json({ success: false, message: "Email ALready Exists" });
-      }
+      res
+        .status(200)
+        .json({ success: true, message: "User Updated Successfully" });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Something Went Wrong,",
+      });
     }
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: "Something Went Wrong, Please Try Afetr Sometime",
-    });
-  }
+  })
 
-  try {
-    const updateUser = await User.findByIdAndUpdate(user._id, {
-      $set: req.body,
-    });
-    console.log("next");
+  // delete User
+  .delete(authVerify, async (req, res) => {
+    const { _id } = req.params;
+    const user = req.user;
+    if (_id !== user._id) {
+      return res.status(200).json({
+        success: false,
+        message: "You Can't Delete Other Users!!",
+      });
+    }
+    try {
+      const deleteUser = await User.deleteOne({ _id });
+      res
+        .status(200)
+        .json({ success: true, message: "user deleted successfully!" });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Something Went Wrong,",
+      });
+    }
+  });
+
+router.route("/:_id/follow").put(authVerify, async (req, res) => {
+  const { _id } = req.params;
+  const user = req.user;
+  if (_id !== user._id) {
+    try {
+      const getCurrentUser = await User.findById(user._id);
+      const getParamsUser = await User.findById(_id);
+      if (!getParamsUser.followers.includes(user._id)) {
+        await getParamsUser.updateOne({ $push: { followers: user._id } });
+        await getCurrentUser.updateOne({ $push: { following: _id } });
+        res
+          .status(200)
+          .josn({ success: true, message: "You Just Followed One User" });
+      } else {
+        res
+          .status(403)
+          .json({ success: false, message: "You Already Follow This User" });
+      }
+    } catch (err) {
+      res.status(500).json({
+        success: false,
+        message: "Something Went Wrong,",
+      });
+    }
+  } else {
     res
-      .status(200)
-      .json({ success: true, message: "User Updated Successfully" });
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      message: "Something Went Wrong,",
-    });
+      .status(403)
+      .json({ success: false, message: "You Can't Follow Yourself!!" });
   }
 });
-
 module.exports = { router };
