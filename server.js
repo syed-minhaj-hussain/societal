b7eb4a5472e8b6e0ddf9b035f194298e5b22c5ca
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-08-30 20:31:11 +0530

    added post route

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const { startConnection } = require("./db.connect");
 const { router: loginRouter } = require("./routes/login.router");
 const { router: registerRouter } = require("./routes/register.router");
 const { router: usersRouter } = require("./routes/users.router");
+const { router: postRouter } = require("./routes/post.router");
 
 startConnection();
 const PORT = process.env.PORT || 8080;
@@ -17,5 +18,6 @@ app.use(express.json());
 app.use("/login", loginRouter);
 app.use("/register", registerRouter);
 app.use("/users", usersRouter);
+app.use("/post", postRouter);
 
 app.listen(PORT, () => console.log("server started at PORT :", PORT));
